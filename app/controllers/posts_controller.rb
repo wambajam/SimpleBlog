631cf19dc17137400ce6742b8ac8dacfcf553881
Michael Beltran <mike.beltran@live.com>
2024-08-01 19:51:29 -0700

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -50,7 +50,6 @@ class PostsController < ApplicationController
   # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy
-
     respond_to do |format|
       format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
       format.json { head :no_content }
